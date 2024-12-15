db1a65d575495c989af2ac0988ebab7276f99082
Daiki-1125 <daiki@Javascript.local>
2024-12-15 23:35:59 +0900

    auto commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  swcMinify: true,       // SWC を使用した高速な minify を有効化
   output: "export",
   images: {
     domains: ['example.com'],
